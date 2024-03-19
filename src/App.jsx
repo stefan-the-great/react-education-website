2ce8d976fe86301e017f8202a4999a8b9c8e26d6
stefanTheGreat <stefanfer01@gmail.com>
2024-03-19 17:59:15 +0530

    All External Assets Downloaded

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { FaGraduationCap } from "react-icons/fa6";
+
+function App() {
+    return (
+        <div className="icon" style={{fontSize: '50px'}}>
+            hello
+            {/* <FaGraduationCap fontSize={'10vh'} /> */}
+        </div>
+    );
+}
+
+export default App;
