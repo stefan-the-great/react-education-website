ce4be47825277018618a467094840502f3e73aca
stefanTheGreat <stefanfer01@gmail.com>
2024-03-23 23:14:28 +0530

    Courses Section Complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ import Microsoft from './images/microsoft-logo.svg';
 import Codecov from './images/codecov.svg';
 import Magicleap from './images/magic-leap-seeklogo.svg';
 import Apple from './images/apple.svg'
+import Person1 from './images/person1.png';
+import Person2 from './images/person2.png';
+import Person3 from './images/person3.png';
+import Person4 from './images/person4.png';
+
+import { PiGraduationCap } from "react-icons/pi";
+import { GoBook } from "react-icons/go";
+import { IoPersonSharp } from "react-icons/io5";
+// import { FaPlay } from "react-icons/fa";
+// import { FaPlay } from "react-icons/fa6";
+import { IoPlay } from "react-icons/io5";
 
 
 
@@ -55,7 +66,68 @@ function App() {
             </section>
             
             <section id='courses'>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea blanditiis perferendis tempore omnis nesciunt voluptas odio natus quam optio earum animi pariatur, accusantium aspernatur temporibus ex corrupti fuga officiis quia?
+                <div className="left">
+                    <div className="outer-box">
+                        <div className="inner-box">
+                            <img src={Person1} alt="" style={{borderBottomRightRadius: '45%'}} />
+                            <img src={Person2} alt="" style={{borderBottomLeftRadius: '45%'}} />
+                            <img src={Person3} alt="" style={{borderTopRightRadius: '45%'}} />
+                            <img src={Person4} alt="" style={{borderTopLeftRadius: '45%'}} />
+                        </div>
+                    </div>
+                </div>
+                <div className="right">
+                    <h2><span id='color-word' style={{color: '#ba73f7'}}>Benefits</span> From Our Online Learning</h2>
+                    <div className="row">
+                        <div className="icon">
+                            <PiGraduationCap fontSize={'36px'} color='white' />
+                        </div>
+                        <div className="text">
+                            <h3>Online Degrees</h3>
+                            <p>Earn accredited degrees from the comfort of your home, opening doors to a world of possibilities.</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="icon">
+                            <GoBook fontSize={'30px'} color='white' />
+                        </div>
+                        <div className="text">
+                            <h3>Short Courses</h3>
+                            <p>Enhance your skills with our concise and focused short courses, designed for quick and effective learning.</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="icon">
+                            <IoPersonSharp fontSize={'26px'} color='white' />
+                        </div>
+                        <div className="text">
+                            <h3>Training From Experts</h3>
+                            <p>Immerse yourself in knowledge with industry experts guiding you through hands-on experience.</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="icon">
+                            <IoPlay fontSize={'30px'} style={{paddingLeft: '5px'}} color='white' />
+                        </div>
+                        <div className="text">
+                            <h3>1.5k + Video Courses</h3>
+                            <p>Dive into a vast library of over 1.5k video courses covering many subjects, offering a visual learning experience.</p>
+                        </div>
+                    </div>
+                    {/* <div className="row">
+                        <div className="icon">
+                            <PiGraduationCap />
+                        </div>
+                        <div className="text">
+                            <h3></h3>
+                            <p></p>
+                        </div>
+                    </div> */}
+                </div>
+            </section>
+
+            <section id='popular-courses'>
+
             </section>
         </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import './navbar.css'
 
 function Navbar() {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+    } else {
+      document.getElementById("header").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
   return (
-    <header>
+    <header id='header'>
         {/* Website Logo */}
         <a href="/" className='logo'>
             Etech.
@@ -12,7 +22,7 @@ function Navbar() {
         {/* Page Links */}
         <nav className='navbar'>
             <ul>
-                <li><a href="#courses">Courses</a></li>
+                <li><a href="#search-courses">Courses</a></li>
                 <li><a href="#teachers">Teachers</a></li>
                 <li><a href="#offers">Offers</a></li>
                 <li><a href="#contact">Contact</a></li>
